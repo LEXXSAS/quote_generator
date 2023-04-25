4527fe7bf5455e3ac0c71cfbfec51d6b59ca799c
Alexey Sedov <ASedov15@gmail.com>
2023-04-25 18:23:11 +0300

    оптимизировал загрузку данных разделив логику рендера и запроса на сервер

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Quote from "./components/Quote";
 import Empty from './components/Empty';
 
 function App() {
   const [quote, setQuote] = useState([]);
+  const [dataQuote, setDataQuote] = useState([]);
 
-  const fetchQuote = async () => {
-    return await fetch('https://raw.githubusercontent.com/rolling-scopes-school/file-storage/random-jokes/quotes.json')
-    .then(response => response.json())
-    .then((data) => {
-      let randomNum = Math.floor(Math.random() * data.length)
-      
-      setQuote(data[randomNum]);
-    })
+  useEffect(() => {
+    const fetchQuote = async () => {
+      return await fetch('https://raw.githubusercontent.com/rolling-scopes-school/file-storage/random-jokes/quotes.json')
+      .then(response => response.json())
+      .then((data) => {
+        setDataQuote(data);
+      })
+    }
+    fetchQuote();
+  }, [])
+
+  const addOneQuote = () => {
+    let randomNum = Math.floor(Math.random() * dataQuote.length)
+    setQuote(dataQuote[randomNum]);
   }
+
 
   return (
     <div className="App">
@@ -21,7 +29,7 @@ function App() {
       
       {(quote.length === 0) ? <Empty /> : <Quote quote={quote} />}
 
-      <button onClick={fetchQuote}>Сгенерировать новую цитату</button>
+      <button onClick={addOneQuote}>Сгенерировать новую цитату</button>
 
     </div>
     </div>
